5c68c5bb76f680e8f69e765f2e8452b68c73b24e
Joel Martinez <joelmartinez@gmail.com>
2015-05-16 15:00:33 -0400

    better function name

--- rprog/pollutantmean.R
+++ rprog/pollutantmean.R
@@ -4,7 +4,7 @@ getfield <- function(data, field)
 	data[field][!is.na(data[field])]
 }
 
-meanforfile <- function(directory, id, pollutant) {
+dataforfile <- function(directory, id, pollutant) {
 	file <- paste(directory, "/", id, sep="")
 	data <- read.csv(file)
 	vec <- getfield(data, pollutant)
@@ -13,6 +13,8 @@ meanforfile <- function(directory, id, pollutant) {
 
 pollutantmean <- function(directory, pollutant, id = 1:332) {
 	files <- dir(directory)[id]
-	means <- unlist(lapply(files, function(f) {meanforfile(directory, f, pollutant)}), use.names=F)
+	means <- unlist(lapply(files, function(f) {
+		dataforfile(directory, f, pollutant)
+	}), use.names=F)
 	mean(c(means))
 }
